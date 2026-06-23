e953140f0a7bc58a470057f1065709c6f34792ec
sanbei101 <sanbei101@outlook.com>
2026-06-23 20:40:52 +0800

    重构为正确的目录

--- src/core/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./ast-similarity";
-export * from "./grammar";
-export * from "./lexer";
-export * from "./llm";
-export * from "./parser";
-export * from "./semantic";
-export * from "./types";
